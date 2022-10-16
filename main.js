14d64925527def9ec4752cfb8bb005332031b5e9
kulotsystems <kulotsystems@gmail.com>
2022-10-17 07:44:24 +0800

    Render first page.

---
+++ main.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Image Resizer',
+        width: 500,
+        height: 600
+    });
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
+        .then(r => {
+            console.log("index loaded: ", r);
+        });
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+});
