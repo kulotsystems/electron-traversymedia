a0d18baccc0a3985395c7ca7ff23f18b066bd09b
kulotsystems <kulotsystems@gmail.com>
2022-10-18 08:01:46 +0800

    Render form.

--- main.js
+++ main.js
@@ -38,7 +38,12 @@ function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Image Resizer',
         width: isDev ? 1200 : 500,
-        height: 600
+        height: 600,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 
     // open devtools if in dev environment
---
+++ renderer/js/renderer.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('#img-form');
+const img  = document.querySelector('#img');
+const outputPath  = document.querySelector('#output-path');
+const filename    = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput  = document.querySelector('#width');
+
+
+// load image
+function loadImage(e) {
+    const file = e.target.files[0];
+    if(!isFileImage(file)) {
+        console.log('Please select an image.');
+        return;
+    }
+
+    // get original dimensions
+    const image = new Image();
+    image.src = URL.createObjectURL(file);
+    image.onload = function() {
+        widthInput.value  = this.width;
+        heightInput.value = this.height;
+    };
+
+    form.style.display   = 'block';
+    filename.innerText   = file.name;
+    outputPath.innerText = path.join(os.homedir(), 'imageresizer');
+}
+
+// check image file
+function isFileImage(file) {
+    const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg'];
+    return file && acceptedImageTypes.includes(file['type']);
+}
+
+
+img.addEventListener('change', loadImage);
